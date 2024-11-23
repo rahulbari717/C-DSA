edb02dfbe7a8993be1dd36242d621cd642746bee
rahulbari <rahulbari717@gmail.com>
2024-11-23 11:33:46 +0530

    recursion codes added...

--- 011Recursion/007TillingProblemInterview.cpp
+++ 011Recursion/007TillingProblemInterview.cpp
@@ -1,21 +1,20 @@
+// Problem Statement: This program calculates the number of ways to tile a 2xn rectangle using 1x2 dominoes.
+// Given an integer n, representing the length of the rectangle, the function tillingProblem(n) is called
+// to compute the total number of distinct arrangements of the dominoes.
+
 #include <iostream>
 using namespace std;
 
-int tillingProblem (int n){     // 2xn 
+int tillingProblem (int n){             // 2xn 
     if (n == 0 || n == 1) {
         return 1;
     }
-    // vertical 
-    int ans1 = tillingProblem(n-1);     // 2xn-1
-
-    // horizontal
-    int ans2 = tillingProblem(n-2);     // 2xn-2
-    return ans1 + ans2;
-
-
-}
+    
+    return tillingProblem(n-1) + tillingProblem(n-2);
+ }
 
 int main () {
+    
     int n = 3;
     cout << tillingProblem(n) <<endl;
     return 0;
